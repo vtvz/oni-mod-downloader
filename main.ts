e7f40b666a1e3a4c95e7c3faf98d5870a408b221
Vitaly <vtvz.ru@gmail.com>
2024-11-24 19:04:38 +0300

    updated

--- main.ts
+++ main.ts
@@ -4,100 +4,7 @@ import fs from "node:fs";
 import path from "node:path";
 import decompress from "npm:decompress";
 import { WorkshopItems } from "./interfaces.ts";
-import { parse, stringify } from "jsr:@std/yaml";
-
-const mods = [
-  /**
-   * Bigger Building Menu
-   * https://steamcommunity.com/sharedfiles/filedetails/?id=1703611962
-   */
-  1703611962,
-  /**
-   * Bigger Camera Zoom Out
-   * https://steamcommunity.com/sharedfiles/filedetails/?id=1717463209
-   */
-  1717463209,
-  /**
-   * Plan Buildings Without Materials
-   * https://steamcommunity.com/sharedfiles/filedetails/?id=1717526174
-   */
-  1717526174,
-  /**
-   * GasOverlay
-   * https://steamcommunity.com/sharedfiles/filedetails/?id=1737859934
-   */
-  1737859934,
-  /**
-   * Suppress Notifications
-   * https://steamcommunity.com/sharedfiles/filedetails/?id=1832319118
-   */
-  1832319118,
-  /**
-   * Build Over Plants
-   * https://steamcommunity.com/sharedfiles/filedetails/?id=1835394161
-   */
-  1835394161,
-  /**
-   * Falling Sand
-   * https://steamcommunity.com/sharedfiles/filedetails/?id=1855163252
-   */
-  1855163252,
-  /**
-   * Sweep By Type
-   * https://steamcommunity.com/sharedfiles/filedetails/?id=1863428350
-   */
-  1863428350,
-  /**
-   * Schedule Master // by @Ony 👾
-   * https://steamcommunity.com/sharedfiles/filedetails/?id=1865119054
-   */
-  1865119054,
-  /**
-   * No Drop // by @Ony 👾
-   * https://steamcommunity.com/sharedfiles/filedetails/?id=1868454866
-   */
-  1868454866,
-  /**
-   * Research Queue
-   * https://steamcommunity.com/sharedfiles/filedetails/?id=1893887916
-   */
-  1893887916,
-  /**
-   * Priority Zero
-   * https://steamcommunity.com/sharedfiles/filedetails/?id=1972768699
-   */
-  1972768699,
-  /**
-   * Deconstruct Only Buildings
-   * https://steamcommunity.com/sharedfiles/filedetails/?id=1992978572
-   */
-  1992978572,
-  /**
-   * Blueprints fixed
-   * https://steamcommunity.com/sharedfiles/filedetails/?id=2435244304
-   */
-  2435244304,
-  /**
-   * Reverse Bridges
-   * https://steamcommunity.com/sharedfiles/filedetails/?id=2982311017
-   */
-  2982311017,
-  /**
-   * Chain Tool
-   * https://steamcommunity.com/sharedfiles/filedetails/?id=3303494244
-   */
-  3303494244,
-  /**
-   * Mass Move Tool
-   * https://steamcommunity.com/sharedfiles/filedetails/?id=3301490799
-   */
-  3301490799,
-  /**
-   * Immigrants Reroll
-   * https://steamcommunity.com/sharedfiles/filedetails/?id=2537508393
-   */
-  2537508393,
-];
+import { parse } from "jsr:@std/yaml";
 
 async function downloadFile(url: string, outputLocationPath: string) {
   const writer = fs.createWriteStream(outputLocationPath);
@@ -173,8 +80,10 @@ async function app() {
   for (const item of resp.data as WorkshopItems) {
     console.log(`${item.publishedfileid} : ${item.title}`);
     const isDisabled = disabledIds.includes(parseInt(item.publishedfileid));
+    const timeUpdated = new Date(item.time_updated * 1000);
 
     modsFileContent.push(`# ${item.title}`);
+    modsFileContent.push(`# ${timeUpdated}`);
     modsFileContent.push(
       `# https://steamcommunity.com/sharedfiles/filedetails/?id=${item.publishedfileid}`,
     );
@@ -209,7 +118,7 @@ async function app() {
       } catch (err) {
         if (attempt == 4) throw err;
 
-        console.log("Caught error", err, `Attempt ${attempt + 2}`);
+        console.log("Caught error", `Attempt ${attempt + 2}`);
       }
     }
 
